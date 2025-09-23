7d1d66d2193cca8d5171afb8007fac2972f435e9
José Valim <jose.valim@dashbit.co>
2025-09-23 09:42:58 +0200

    Docs

--- src/tidewave/django/__init__.py
+++ src/tidewave/django/__init__.py
@@ -35,6 +35,16 @@ class Middleware(MiddlewareMixin):
         - ALLOWED_HOSTS: Used as allowed origins
         - TIDEWAVE['allow_remote_access']: Whether to allow remote connections (default False)
         - TIDEWAVE['team']: Enable Tidewave for teams
+
+    Optional configuration:
+        If you are using Jinja2 with Django, you need to explicitly add
+
+        JINJA2_ENVIRONMENT_OPTIONS = {
+            "extensions": [
+                "tidewave.jinja2.Extension"
+            ],
+        }
+
     """
 
     def __init__(self, get_response: Callable):
