1986b49de34d08c4150d2c284aa2ab67ac5d4f81
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-12-18 18:17:20 +0100

    Fix FastAPI redirecting /tidewave to /tidewave/

--- src/tidewave/fastapi/middleware.py
+++ src/tidewave/fastapi/middleware.py
@@ -7,8 +7,24 @@ from tidewave.middleware import modify_csp
 
 class Middleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        request = self._process_requset(request)
         response = await call_next(request)
         return self._process_response(response)
+
+    def _process_requset(self, request):
+        # By default starlette redirects paths to their trailing slash
+        # counterparts. To avoid such redirects for tidewave, we rewrite
+        # the request path. See [1].
+        #
+        # Redirects are problematic, if there is a proxy in front of
+        # the app, such as Vite dev server. The proxy may not rewrite
+        # the redirect origin, so when the browser follows the redirect
+        # it skips the proxy.
+        #
+        # [1]: https://github.com/Kludex/starlette/issues/869#issuecomment-908634218
+        if request.scope["path"] == "/tidewave":
+            request.scope["path"] = "/tidewave/"
+            request.scope["raw_path"] = b"/tidewave/"
 
     def _process_response(self, response):
         """
